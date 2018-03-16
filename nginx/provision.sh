f4951efe632d767e45af745dcb425705b86856cb
xuweicheng <weicheng.xu@gmail.com>
2018-03-16 09:04:12 -0700

    initilize vagrant prototype

---
+++ nginx/provision.sh
@@ -0,0 +1,11 @@
+apt-get -y update
+
+apt-get -y install nginx
+
+rm -rf /etc/nginx/sites-enabled
+cp -r /vagrant/sites-enabled /etc/nginx
+
+# rm -rf /usr/share/nginx/www
+# ln -s /vagrant/www /usr/share/nginx/www
+
+service nginx start
